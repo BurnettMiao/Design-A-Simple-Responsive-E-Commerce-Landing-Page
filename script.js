f6ffd216a211e5636bde3a88ac4653fac5cf9185
BurnettMiao <searchfor1987@gmail.com>
2023-11-10 17:31:47 +0800

    project finish

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const trendingSwiper = new Swiper('.trending__swiper', {
   slidesPerView: 'auto',
   spaceBetween: 50,
 });
+
+const testimonialSwiper = new Swiper('.testimonial__swiper', {
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
